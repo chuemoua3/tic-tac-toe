55e0fdac57c8baaa9ccecb78ed0c72eb86ca3e12
Chue <chuemoua12@outlook.com>
2020-04-27 18:19:35 -0400

    Updates

---
+++ precache-manifest.2b89135dba11d68daa06d5e85f9fca56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454d5d48a222061e995dd9454fdae9c0",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "99c88151ccc8572dd7ab",
+    "url": "/tic-tac-toe/static/css/main.614c50db.chunk.css"
+  },
+  {
+    "revision": "db7511ef2fcc00df4587",
+    "url": "/tic-tac-toe/static/js/2.4fd1cbe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.4fd1cbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c88151ccc8572dd7ab",
+    "url": "/tic-tac-toe/static/js/main.f643e472.chunk.js"
+  },
+  {
+    "revision": "584ea65a4be99bc11608",
+    "url": "/tic-tac-toe/static/js/runtime-main.29647a31.js"
+  }
+]);
